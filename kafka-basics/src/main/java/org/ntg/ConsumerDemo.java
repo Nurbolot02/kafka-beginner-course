1797aa827ae067325fc8ae6b80535cd430df0e88
nurbolot <ngulamidinov@gmail.com>
2023-01-24 03:11:43 +0600

    add ConsumerDemo

---
+++ kafka-basics/src/main/java/org/ntg/ConsumerDemo.java
@@ -0,0 +1,49 @@
+package org.ntg;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+public class ConsumerDemo {
+    private static final Logger log = LoggerFactory.getLogger(ConsumerDemo.class);
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "kafka");
+        //properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+        String topic = "ntg";
+        consumer.subscribe(List.of(topic));
+
+        long start = System.currentTimeMillis();
+        int i = 0;
+
+        while (i <= 100) {
+            ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(100));
+            Iterable<ConsumerRecord<String, String>> records = consumerRecords.records(topic);
+            for (ConsumerRecord<String, String> record: records) {
+                log.info(String.format("\n\nPartition: %s \nKey: %s \n Value: %s \nOffset: %s \nTimeStamp: %s\n",
+                        record.partition(),record.key(), record.value(), record.offset(), record.timestamp()));
+                i++;
+            }
+        }
+
+
+        long end = System.currentTimeMillis();
+        log.info(String.format("Time: %d", end - start));
+
+        // consumer.close();
+    }
+}
